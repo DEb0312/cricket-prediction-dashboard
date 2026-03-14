8fa560018c9890bc8426c0546e261f3d7b85df91
Debraj0312 <chakrabortydola03@gmail.com>
2026-03-15 01:49:14 +0530

    fixed render server startup

--- server.js
+++ server.js
@@ -1,13 +1,14 @@
+require("dotenv").config()
+
 const express = require("express")
 const mongoose = require("mongoose")
 const bodyParser = require("body-parser")
 const cors = require("cors")
 
-require("dotenv").config()
-
 const authRoutes = require("./routes/auth")
 const predictionRoutes = require("./routes/prediction")
 const leaderboardRoutes = require("./routes/leaderboard")
+const resultRoutes = require("./routes/result")
 
 const app = express()
 
@@ -17,13 +18,15 @@ app.use(express.static("public"))
 
 mongoose.connect(process.env.MONGO_URI)
 .then(()=>console.log("MongoDB connected"))
+.catch(err=>console.log(err))
 
 app.use("/api/auth", authRoutes)
 app.use("/api/prediction", predictionRoutes)
 app.use("/api/leaderboard", leaderboardRoutes)
-
-app.listen(5000, () => {
-    console.log("Server running on port 5000")
-})
-const resultRoutes = require("./routes/result")
 app.use("/api/result", resultRoutes)
+
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT, () => {
+    console.log("Server running on port", PORT)
+})
