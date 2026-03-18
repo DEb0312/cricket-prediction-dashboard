808116ec28ce7499e9282ba456754bf4a5ea2ebf
Debraj0312 <chakrabortydola03@gmail.com>
2026-03-18 13:05:37 +0530

    add fix route

--- server.js
+++ server.js
@@ -30,3 +30,10 @@ const PORT = process.env.PORT || 5000
 app.listen(PORT, () => {
     console.log("Server running on port", PORT)
 })
+
+const Prediction = require("./models/Prediction")
+
+app.get("/fix-processed", async (req, res) => {
+  await Prediction.updateMany({}, { processed: false })
+  res.send("All predictions updated with processed=false")
+})
